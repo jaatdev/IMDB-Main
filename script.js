75628c4b817b9fd08790408dd38cb779620e0f8a
jaatdev <48481927+jaatdev@users.noreply.github.com>
2023-08-20 19:14:34 +0530

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
+Certainly, here's the complete corrected JavaScript code:
+
+```javascript
 const movieSearchBox = document.getElementById('movie-search-box');
 const searchList = document.getElementById('search-list');
 const resultGrid = document.getElementById('result-grid');
 
 // load movies from API
 async function loadMovies(searchTerm) {
-    const URL = `https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=9483c861`;
-
+    const URL = `https://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=9483c861`;
     const res = await fetch(URL);
     const data = await res.json();
-    
-    if (data.Response === "True") {
+    if (data.Response == "True") {
         displayMovieList(data.Search);
     }
 }
@@ -30,17 +31,16 @@ function displayMovieList(movies) {
         let movieListItem = document.createElement('div');
         movieListItem.dataset.id = movies[idx].imdbID;
         movieListItem.classList.add('search-list-item');
-
-        const moviePoster = movies[idx].Poster !== "N/A" ? movies[idx].Poster : "image_not_found.png";
+        let moviePoster = (movies[idx].Poster != "N/A") ? movies[idx].Poster : "image_not_found.png";
 
         movieListItem.innerHTML = `
-            <div class="search-item-thumbnail">
-                <img src="${moviePoster}">
-            </div>
-            <div class="search-item-info">
-                <h3>${movies[idx].Title}</h3>
-                <p>${movies[idx].Year}</p>
-            </div>
+        <div class="search-item-thumbnail">
+            <img src="${moviePoster}">
+        </div>
+        <div class="search-item-info">
+            <h3>${movies[idx].Title}</h3>
+            <p>${movies[idx].Year}</p>
+        </div>
         `;
         searchList.appendChild(movieListItem);
     }
@@ -62,28 +62,31 @@ function loadMovieDetails() {
 
 function displayMovieDetails(details) {
     resultGrid.innerHTML = `
-        <div class="movie-poster">
-            <img src="${details.Poster !== "N/A" ? details.Poster : "image_not_found.png"}" alt="movie poster">
-        </div>
-        <div class="movie-info">
-            <h3 class="movie-title">${details.Title}</h3>
-            <ul class="movie-misc-info">
-                <li class="year">Year: ${details.Year}</li>
-                <li class="rated">Ratings: ${details.Rated}</li>
-                <li class="released">Released: ${details.Released}</li>
-            </ul>
-            <p class="genre"><b>Genre:</b> ${details.Genre}</p>
-            <p class="writer"><b>Writer:</b> ${details.Writer}</p>
-            <p class="actors"><b>Actors: </b>${details.Actors}</p>
-            <p class="plot"><b>Plot:</b> ${details.Plot}</p>
-            <p class="language"><b>Language:</b> ${details.Language}</p>
-            <p class="awards"><b><i class="fas fa-award"></i></b> ${details.Awards}</p>
-        </div>
+    <div class="movie-poster">
+        <img src="${(details.Poster != "N/A") ? details.Poster : "image_not_found.png"}" alt="movie poster">
+    </div>
+    <div class="movie-info">
+        <h3 class="movie-title">${details.Title}</h3>
+        <ul class="movie-misc-info">
+            <li class="year">Year: ${details.Year}</li>
+            <li class="rated">Ratings: ${details.Rated}</li>
+            <li class="released">Released: ${details.Released}</li>
+        </ul>
+        <p class="genre"><b>Genre:</b> ${details.Genre}</p>
+        <p class="writer"><b>Writer:</b> ${details.Writer}</p>
+        <p class="actors"><b>Actors: </b>${details.Actors}</p>
+        <p class="plot"><b>Plot:</b> ${details.Plot}</p>
+        <p class="language"><b>Language:</b> ${details.Language}</p>
+        <p class="awards"><b><i class="fas fa-award"></i></b> ${details.Awards}</p>
+    </div>
     `;
 }
 
 window.addEventListener('click', (event) => {
-    if (event.target.className !== "form-control") {
+    if (event.target.className != "form-control") {
         searchList.classList.add('hide-search-list');
     }
 });
+```
+
+Make sure to replace the existing JavaScript code in your project with this corrected code. This should resolve the mixed content issue and other errors you were encountering.
