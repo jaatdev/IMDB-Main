484e2b5122778a3879215a13edfdc8320293bf73
jaatdev <48481927+jaatdev@users.noreply.github.com>
2023-08-20 18:50:20 +0530

    script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function loadMovieDetails() {
         movie.addEventListener('click', async () => {
             searchList.classList.add('hide-search-list');
             movieSearchBox.value = "";
-            const result = await fetch(`https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=9483c861`);
+            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=9483c861`);
             const movieDetails = await result.json();
             displayMovieDetails(movieDetails);
         });
